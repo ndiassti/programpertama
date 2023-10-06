9e88ce16a901613c2230c7d222691bd42821b330
Nadia Sasti <147120494+ndiassti@users.noreply.github.com>
2023-10-06 10:32:22 +0700

    Add files via upload

---
+++ Nadia Sasti.py
@@ -0,0 +1,6 @@
+print('Nama Nadia Sasti')
+print ('Alamat saya desa kacung')
+print ('Hobi saya nonton drakor dan juga memasak')
+print ('Asal sekolah SMKN 1 Kelapa')
+print ('Saya lahir pada tanggal 16 Juni 2005')
+
